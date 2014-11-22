f3b3f7a353a55ec0262dd85ff66347c7d67e8a38
Jon Binney <jon.binney@gmail.com>
2014-11-22 12:42:14 -0800

    Start ROSNode struct

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,14 @@ use std::io::TcpStream;
 use std::fmt;
 use std::string;
 
-fn execute_xmlrpc_request(stream: &mut TcpStream, request: &String) -> String {
+struct ROSNode {
+    master_uri: String
+}
+
+
+fn execute_xmlrpc_request(server_uri: &str, request: &str) -> String {
+
+    let mut stream = TcpStream::connect(server_uri).unwrap();
 
     let header = format!(
         "POST /RPC2 HTTP/1.0\n\
@@ -11,7 +18,7 @@ fn execute_xmlrpc_request(stream: &mut TcpStream, request: &String) -> String {
         Content-Type: text/xml\n\
         Content-length: {content_length}\n\n", content_length=request.len());
 
-    let message = header + *request;
+    let message = header + request.to_string();
 
     println!("request: {}", message);
 
@@ -19,24 +26,30 @@ fn execute_xmlrpc_request(stream: &mut TcpStream, request: &String) -> String {
     stream.read_to_string().unwrap()
 }
 
+impl ROSNode {
+    fn get_published_topics(&self) -> String
+    {
+        let request =
+            "<?xml version=\"1.0\"?>\n\
+            <methodCall>\n\
+            <methodName>getPublishedTopics</methodName>\n\
+            <params>\n\
+            <param>\n\
+            <value><string>meeeee</string></value>\n\
+            <value><string></string></value>\n\
+            </param>\n\
+            </params>\n\
+            </methodCall>\n".to_string();
+
+        execute_xmlrpc_request(self.master_uri.as_slice(), request.as_slice())
+    }
+}
+
 fn main() {
     println!("Starting node");
+    let node = ROSNode {master_uri: String::from_str("localhost:11311")};
 
-    let mut stream = TcpStream::connect("localhost:11311").unwrap();
-
-    let request =
-        "<?xml version=\"1.0\"?>\n\
-        <methodCall>\n\
-          <methodName>getPublishedTopics</methodName>\n\
-          <params>\n\
-            <param>\n\
-                <value><string>meeeee</string></value>\n\
-                <value><string></string></value>\n\
-            </param>\n\
-          </params>\n\
-        </methodCall>\n".to_string();
-
-    let response = execute_xmlrpc_request(&mut stream, &request);
+    let response = node.get_published_topics();
     println!("response: {}", response);
 }
 
